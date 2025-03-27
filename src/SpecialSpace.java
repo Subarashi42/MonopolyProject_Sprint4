5a95e78170110b92af955b4505bd7cd4aba35300
Marena-AB <abboudm@moravian.edu>
2025-03-27 11:55:37 -0400

    Fixed issue in SpecialSpace class

--- src/SpecialSpace.java
+++ src/SpecialSpace.java
@@ -1,6 +1,6 @@
 public class SpecialSpace extends Space {
     public SpecialSpace(String name, int position, String type){
-        super(name, position, "Property");
+        super(name, position, type);
     }
 
     public String getType() {
