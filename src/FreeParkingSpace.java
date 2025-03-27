fc2cad9373545f969efb85125f91d67cdd4c54fe
Marena-AB <abboudm@moravian.edu>
2025-03-27 10:09:01 -0400

    implemented FreeParkingSpace class

--- src/FreeParkingSpace.java
+++ src/FreeParkingSpace.java
@@ -1,2 +1,65 @@
-public class FreeParkingSpace {
+public class FreeParkingSpace extends SpecialSpace {
+    private int moneyPool; // Optional: for house rule where money from fines goes to Free Parking
+
+    /**
+     * Constructs a Free Parking space.
+     * In standard Monopoly, landing on Free Parking has no effect.
+     */
+    public FreeParkingSpace() {
+        super("Free Parking", 20, "Free Parking");
+        this.moneyPool = 0;
+    }
+
+    /**
+     * Handles a player landing on the Free Parking space.
+     * By default, nothing happens in standard rules.
+     * But a common house rule is that the player collects any money in the pot.
+     *
+     * @param player The player who landed on Free Parking
+     * @param gameState The current game state
+     * @param useHouseRules Whether to use the house rule where player collects money
+     */
+    public void onLand(Player player, GameState gameState, boolean useHouseRules) {
+        System.out.println(player.getName() + " landed on Free Parking.");
+
+        if (useHouseRules && moneyPool > 0) {
+            System.out.println(player.getName() + " collects $" + moneyPool + " from Free Parking!");
+            player.addMoney(moneyPool);
+            moneyPool = 0; // Reset the money pool
+        }
+    }
+
+    /**
+     * Adds money to the Free Parking pool.
+     * This would be used with house rules that put fines and taxes into the pot.
+     *
+     * @param amount The amount of money to add to the pool
+     */
+    public void addMoneyToPool(int amount) {
+        moneyPool += amount;
+    }
+
+    /**
+     * Gets the current amount of money in the Free Parking pool.
+     *
+     * @return The amount of money in the pool
+     */
+    public int getMoneyPool() {
+        return moneyPool;
+    }
+
+    /**
+     * Default version of onLand that uses standard rules (no money collection).
+     *
+     * @param player The player who landed on Free Parking
+     * @param gameState The current game state
+     */
+    public void onLand(Player player, GameState gameState) {
+        onLand(player, gameState, false);
+    }
+
+    @Override
+    public void playerOnSpecialSpace() {
+        System.out.println("Player landed on Free Parking");
+    }
 }
