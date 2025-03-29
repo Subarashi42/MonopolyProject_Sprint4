aa903cfc068b342ac7fb5a705a2d6261559a2cd0
Marena-AB <abboudm@moravian.edu>
2025-03-29 08:54:19 -0400

    Enhanced GameState Class

--- src/GameState.java
+++ src/GameState.java
@@ -1,10 +1,3 @@
-import java.util.List;
-import java.util.Map;
-
-/**
- * Interface to represent the state of a Monopoly game.
- * Tracks players, board, dice, cards and overall game status.
- */
 import java.util.*;
 
 public class GameState {
@@ -16,21 +9,41 @@ public class GameState {
     private ChanceCards chanceCards;
     private Map<Player, Boolean> isInJail;
     private boolean gameActive;
+    private Bank bank; // Added Bank reference
 
     public GameState(List<Player> players, Gameboard board) {
-            this.players = players; // Initialize players
-            this.board = new Gameboard();  // Initialize board
-            this.dice = new Dice();
-            this.communityChestCards = new CommunityChestCards();
-            this.chanceCards = new ChanceCards();
-            this.isInJail = new HashMap<>();
-            for (Player player : this.players) {
-                isInJail.put(player, false);
-            }
-            this.gameActive = true;
-            this.currentPlayerIndex = 0;
-            communityChestCards.cards();
-            chanceCards.cards();
+        this.players = players; // Initialize players
+        this.board = board;  // Use the provided board
+        this.dice = new Dice();
+        this.communityChestCards = new CommunityChestCards();
+        this.chanceCards = new ChanceCards();
+        this.isInJail = new HashMap<>();
+        for (Player player : this.players) {
+            isInJail.put(player, false);
+        }
+        this.gameActive = true;
+        this.currentPlayerIndex = 0;
+        communityChestCards.cards();
+        chanceCards.cards();
+        // Bank will be set separately
+    }
+
+    /**
+     * Gets the bank for this game.
+     *
+     * @return The bank
+     */
+    public Bank getBank() {
+        return bank;
+    }
+
+    /**
+     * Sets the bank for this game.
+     *
+     * @param bank The bank to use
+     */
+    public void setBank(Bank bank) {
+        this.bank = bank;
     }
 
     /**
@@ -139,13 +152,42 @@ public class GameState {
                 (isPlayerInJail(player) ? " (In Jail)" : ""));
     }
 
-    public void skipBankrupt(Player alice) {
-        players.remove(alice);
+    /**
+     * Handles a player going bankrupt.
+     * This includes returning all properties to the bank and removing the player from the game.
+     *
+     * @param player The bankrupt player
+     */
+    public void handlePlayerBankruptcy(Player player) {
+        System.out.println(player.getName() + " is bankrupt and out of the game!");
+
+        // Return all properties to the bank
+        for (Property property : player.getProperties()) {
+            property.setOwner(null);
+            property.setHouses(0);
+            property.setHasHotel(false);
+            property.setMortgaged(false);
+        }
+
+        // Remove player from the game
+        players.remove(player);
+        isInJail.remove(player);
+
+        // Check if game is over
+        if (players.size() == 1) {
+            System.out.println(players.get(0).getName() + " wins the game!");
+            gameActive = false;
+        }
+    }
+
+    public void skipBankrupt(Player player) {
+        players.remove(player);
     }
 
     public Object getDice() {
         return dice;
     }
+
     public void setDice(Dice dice) {
         this.dice = dice;
     }
@@ -153,15 +195,19 @@ public class GameState {
     public CommunityChestCards getCommunityChestCards() {
         return communityChestCards;
     }
+
     public ChanceCards getChanceCards() {
         return chanceCards;
     }
+
     public void setGameActive(boolean gameActive) {
         this.gameActive = gameActive;
     }
+
     public void setPlayers(List<Player> players) {
         this.players = players;
     }
+
     public void setBoard(Gameboard board) {
         this.board = board;
     }
@@ -178,5 +224,3 @@ public class GameState {
         return currentPlayerIndex;
     }
 }
-
-
