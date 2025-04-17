fa44b3997bd680d7899d186414bfa573e7b5fff7
Marena-AB <abboudm@moravian.edu>
2025-04-17 10:00:40 -0400

    Deleted unused methods

--- src/View/GUI.java
+++ src/View/GUI.java
@@ -646,94 +646,6 @@ public class GUI extends JFrame {
         dicePanel.repaint();
     }
 
-    /**
-     * Author: Marena
-     * Creates a visual representation of a die with the specified value
-     */
-    private JPanel createDiePanel(int value) {
-        JPanel diePanel = new JPanel() {
-            @Override
-            public Dimension getPreferredSize() {
-                return new Dimension(60, 60);
-            }
-
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                Graphics2D g2d = (Graphics2D) g;
-                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-                // Draw die face (white rounded square with black border)
-                g2d.setColor(Color.WHITE);
-                g2d.fillRoundRect(2, 2, getWidth() - 4, getHeight() - 4, 10, 10);
-                g2d.setColor(Color.BLACK);
-                g2d.setStroke(new BasicStroke(2));
-                g2d.drawRoundRect(2, 2, getWidth() - 4, getHeight() - 4, 10, 10);
-
-                // Draw dots based on value
-                g2d.setColor(Color.BLACK);
-                int dotSize = getWidth() / 6;  // Slightly smaller dots
-
-                // Calculate positions
-                int margin = getWidth() / 6;
-                int center = getWidth() / 2 - dotSize / 2;
-                int left = margin - dotSize / 2;
-                int right = getWidth() - margin - dotSize / 2;
-                int top = margin - dotSize / 2;
-                int bottom = getHeight() - margin - dotSize / 2;
-
-                switch (value) {
-                    case 1:
-                        // Center dot
-                        g2d.fillOval(center, center, dotSize, dotSize);
-                        break;
-                    case 2:
-                        // Top-left and bottom-right
-                        g2d.fillOval(left, top, dotSize, dotSize);
-                        g2d.fillOval(right, bottom, dotSize, dotSize);
-                        break;
-                    case 3:
-                        // Top-left, center, and bottom-right
-                        g2d.fillOval(left, top, dotSize, dotSize);
-                        g2d.fillOval(center, center, dotSize, dotSize);
-                        g2d.fillOval(right, bottom, dotSize, dotSize);
-                        break;
-                    case 4:
-                        // Four corners
-                        g2d.fillOval(left, top, dotSize, dotSize);
-                        g2d.fillOval(right, top, dotSize, dotSize);
-                        g2d.fillOval(left, bottom, dotSize, dotSize);
-                        g2d.fillOval(right, bottom, dotSize, dotSize);
-                        break;
-                    case 5:
-                        // Four corners plus center
-                        g2d.fillOval(left, top, dotSize, dotSize);
-                        g2d.fillOval(right, top, dotSize, dotSize);
-                        g2d.fillOval(center, center, dotSize, dotSize);
-                        g2d.fillOval(left, bottom, dotSize, dotSize);
-                        g2d.fillOval(right, bottom, dotSize, dotSize);
-                        break;
-                    case 6:
-                        // Six dots (three on each side)
-                        g2d.fillOval(left, top, dotSize, dotSize);
-                        g2d.fillOval(left, center, dotSize, dotSize);
-                        g2d.fillOval(left, bottom, dotSize, dotSize);
-                        g2d.fillOval(right, top, dotSize, dotSize);
-                        g2d.fillOval(right, center, dotSize, dotSize);
-                        g2d.fillOval(right, bottom, dotSize, dotSize);
-                        break;
-                }
-            }
-        };
-
-        // Set background too transparent to better show the die face
-        diePanel.setOpaque(false);
-
-        // Add a subtle border to the panel itself
-        diePanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-
-        return diePanel;
-    }
 
     /**
      * Author: Marena
@@ -804,36 +716,6 @@ public class GUI extends JFrame {
         boardPanel.repaint();
     }
 
-    /**
-     * Author: Marena
-     * Shows a die roll animation effect
-     */
-    private void showDiceRollAnimation(final int finalDie1, final int finalDie2) {
-        // Create timer for animation (rolls dice quickly several times)
-        final int animationFrames = 10; // Number of animation frames
-        final int delay = 50; // Milliseconds between frames
-
-        final Timer timer = new Timer(delay, null);
-        final int[] frameCount = {0};
-
-        timer.addActionListener(e -> {
-            if (frameCount[0] < animationFrames) {
-                // Show random dice during animation
-                lastDiceRoll[0] = (int) (Math.random() * 6) + 1;
-                lastDiceRoll[1] = (int) (Math.random() * 6) + 1;
-                updateDiceDisplay();
-                frameCount[0]++;
-            } else {
-                // Show final dice values
-                lastDiceRoll[0] = finalDie1;
-                lastDiceRoll[1] = finalDie2;
-                updateDiceDisplay();
-                timer.stop();
-            }
-        });
-
-        timer.start();
-    }
 
     /**
      * Author: Marena
@@ -1389,8 +1271,6 @@ public class GUI extends JFrame {
          * Constructor for BoardPanel
          * Sets up the board size, background color, and loads images
          */
-
-        // In the BoardPanel constructor:
         public BoardPanel() {
             setPreferredSize(new Dimension(BOARD_SIZE, BOARD_SIZE));
             setMinimumSize(new Dimension(BOARD_SIZE, BOARD_SIZE));
